7cae727343c89b42f09114847410c64fa9054706
myk_tov <mykyta.tovstyi@gmail.com>
2026-06-25 22:57:47 +0000

    Refactor bfs function to accept session as a parameter; enhance main loop for user input and exit handling

--- crawler.py
+++ crawler.py
@@ -45,53 +45,63 @@ def is_valid_wiki_link(href: str) -> bool:
     return True
 
 
-async def bfs(start_url: str, max_moves: int = 6) -> list:
+async def bfs(start_url: str, session: aiohttp.ClientSession, max_moves: int = 6) -> list:
     if start_url == TARGET:
         return [start_url]
     
     used = set([start_url])
     current_lvl = [(start_url, [start_url])]
-    
-    async with aiohttp.ClientSession(headers= HEADERS) as session:
-        sem = asyncio.Semaphore(100)
-        for move in range(max_moves):
-            tasks = []
 
-            for node in current_lvl:
-                task = extract_links(node[0], session, sem)
-                tasks.append(task)
+    sem = asyncio.Semaphore(100)
+    for move in range(max_moves):
+        tasks = []
+
+        for node in current_lvl:
+            task = extract_links(node[0], session, sem)
+            tasks.append(task)
             
-            results = await asyncio.gather(*tasks)
+        results = await asyncio.gather(*tasks)
 
-            next_lvl = []
+        next_lvl = []
 
-            for parent, child_urls in zip(current_lvl, results):
-                parent_url, parent_path = parent
+        for parent, child_urls in zip(current_lvl, results):
+            parent_url, parent_path = parent
 
-                for child in child_urls:
-                    if child == TARGET:
-                        return parent_path + [child]
+            for child in child_urls:
+                if child == TARGET:
+                    return parent_path + [child]
                     
-                    if child not in used:
-                        used.add(child)
-                        next_lvl.append((child, parent_path + [child]))
+                if child not in used:
+                    used.add(child)
+                    next_lvl.append((child, parent_path + [child]))
             
-            current_lvl = next_lvl
+        current_lvl = next_lvl
 
     return None
 
 
 async def main():
-    initial_link = input("Input the wikipedia link: ")
-    print(f"Starting search with the {initial_link}")
-    path = await bfs(initial_link)
+    print("Input the word 'exit' to leave the program.")
+    async with aiohttp.ClientSession(headers=HEADERS) as session:
+        while True:
+            initial_link = input("Input the wikipedia link: ")
 
-    if path:
-        print("Path exists! You have to go through: ")
-        print(" ->\n".join(path))
+            if initial_link.lower() == 'exit':
+                print("Goodbye!")
+                break
+            if not initial_link:
+                continue
+            
 
-    else:
-        print("Hitler not found")
+            print(f"Starting search with the {initial_link}")
+            path = await bfs(initial_link, session)
+
+            if path:
+                print("Path exists! You have to go through: ")
+                print(" ->\n".join(path))
+
+            else:
+                print("Hitler not found")
 
 
 if __name__ == "__main__":
